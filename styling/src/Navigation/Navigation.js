5e5a45e1b9b00c01069c36046a6b7d3ccd37bc75
pinki <pinki.bhati@tothenew.com>
2020-05-11 09:02:52 +0530

    styling

--- styling/src/Navigation/Navigation.js
+++ styling/src/Navigation/Navigation.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import classes from './navigation.module.css'
 
- class Navigation extends Component {
+class Navigation extends Component {
     render() {
         return (
-            <nav>
-                <ul>
-                    <NavLink  to="/radium">
-                        <li className={classes.Links}>Radium</li>
+            <nav className={classes.Nav}>
+                <ul className={classes.Links}>
+                    <NavLink to="/radium">
+                        <li >Radium</li>
                     </NavLink>
-                    
-                    <NavLink  to="/styledComponents">
-                        <li className={classes.Links}>StyledComponent</li>
+
+                    <NavLink to="/styledComponents">
+                        <li>StyledComponent</li>
                     </NavLink>
-                    <NavLink  to="/cssModule">
-                        <li className={classes.Links}>CssModule</li>
+                    <NavLink to="/cssModule">
+                        <li >CssModule</li>
                     </NavLink>
                     <NavLink to="/inlineCss">
-                        <li className={classes.Links}>InlineCss</li>
+                        <li >InlineCss</li>
                     </NavLink>
                 </ul>
             </nav>
